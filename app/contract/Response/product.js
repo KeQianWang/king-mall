2a7d40dba317be653d6e834aa3009437dbb9e814
moom <junjihe@live.com>
2020-12-18 23:43:18 +0800

    修改配置

--- app/contract/Response/product.js
+++ app/contract/Response/product.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     JsonBody: { // 这个名字对应上面 Controller 注释的@response 的 JsonBody。
-        result: { type: "string",required: true}, // 服务器返回的数据。
-    },
+        result: { type: 'string', required: true } // 服务器返回的数据。
+    }
 };
--- app/service/product.js
+++ app/service/product.js
@@ -3,14 +3,14 @@
 /**
  * @Service product
  */
+
 const Service = require('egg').Service;
 
 // 通过定义 Service 类的方式来编写代码，所有的 Service 文件都必须放在 app/service 目录下。
 class ProductServices extends Service {
-
     async show() {
         const product = await this.app.mysql.get('product');
-        return product
+        return product;
     }
 }
 

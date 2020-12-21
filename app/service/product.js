b4688fc914cf8c41ff0dfca2a9831ae503407287
moom <junjihe@live.com>
2020-12-21 22:13:45 +0800

    删除 product 先判断是否存在 增加格式化vscode 支持

--- app/service/product.js
+++ app/service/product.js
@@ -8,18 +8,23 @@ const Service = require('egg').Service;
 
 // 通过定义 Service 类的方式来编写代码，所有的 Service 文件都必须放在 app/service 目录下。
 class ProductServices extends Service {
-
     async findProducts(query) {
         const { ctx } = this;
-        query.order = [[ 'created_at', 'desc' ], [ 'id', 'desc' ]];
+        query.order = [
+            ['createdAt', 'desc'],
+            ['id', 'desc'],
+        ];
         const found_product = await ctx.model.Product.findAndCountAll(query);
         return found_product;
     }
 
-    async destroyProduct(query) {
+    async destroyProduct(id) {
         const { ctx } = this;
-        const delete_product = await ctx.model.Product.destroy(query);
-        return delete_product;
+        const product = await ctx.model.Product.findByPk(id);
+        if (!product) {
+            this.ctx.throw(404, 'product not found!');
+        }
+        return await product.destroy();
     }
 }
 
--- config/plugin.js
+++ config/plugin.js
@@ -20,6 +20,17 @@ module.exports = {
     },
     sequelize: {
         enable: true,
-        package: 'egg-sequelize'
+        package: 'egg-sequelize',
+        define:{
+            // 主动写入工夫戳 created_at updated_at
+            timestamps: true,
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+            // 字段生成软删除工夫戳 deleted_at
+            // paranoid: true,
+            deletedAt: 'deleted_at',
+            // 所有驼峰命名格式化
+            underscored: true
+        }
     }
 };

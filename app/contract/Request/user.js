dbf2d64e7cf57a74deeeb9cac25f349fff0e1584
zhangliang <liang.zhang@biomind.ai>
2020-12-22 18:18:52 +0800

    添加新增用户接口

---
+++ app/contract/Request/user.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    userRequest: {
+        name: { type: 'string', required: true, description: '名称' },
+        age: { type: 'number', required: false, description: '年龄' },
+    },
+};
---
+++ app/controller/users.js
@@ -0,0 +1,25 @@
+/* egg_folder/app/controller/users.js */
+'use strict';
+/**
+ * @Controller users
+ */
+const Controller = require('egg').Controller;
+
+class UsersController extends Controller {
+    /**
+     * @summary 创建用户
+     * @description 创建创建用户
+     * @router POST /api/users
+     * @request body userRequest *body 产品信息
+     * @response 201 createUserResponse 创建成功
+     */
+    async create() {
+        const ctx = this.ctx;
+        const { name, age } = ctx.request.body;
+        const res = await ctx.model.User.create({ name, age });
+        ctx.status = 201;
+        ctx.body = res;
+    }
+}
+
+module.exports = UsersController;

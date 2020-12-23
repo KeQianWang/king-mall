d8362c1b2e4ed0d46ccf96f549c9bbd5e26c86ec
zhangliang <liang.zhang@biomind.ai>
2020-12-23 17:35:47 +0800

    用户接口修改

--- app/model/user.js
+++ app/model/user.js
@@ -1,12 +1,14 @@
 'use strict';
 
-module.exports = app => {
-    const { STRING, INTEGER, DATE } = app.Sequelize;
+module.exports = (app) => {
+    const { STRING, INTEGER, DATE, BIGINT } = app.Sequelize;
 
     const User = app.model.define('user', {
         id: { type: INTEGER, primaryKey: true, autoIncrement: true },
         name: STRING(30),
         age: INTEGER,
+        gender: STRING,
+        cellphone: BIGINT(11),
         createdAt: DATE,
         updatedAt: DATE
     });

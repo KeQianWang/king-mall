8577f8e6ac036892ed1e78c9939df64ade2fec19
hejunji <junji.he@biomind.ai>
2020-12-24 10:58:56 +0800

    数据库sequelize 优化 3, error fix

---
+++ database/migrations/20201224025507-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING
+            },
+            age: {
+                type: Sequelize.INTEGER
+            },
+            avatar: {
+                type: Sequelize.STRING
+            },
+            gender: {
+                type: Sequelize.STRING
+            },
+            cellphone: {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('users');
+    }
+};

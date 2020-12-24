2a205e5b3655eee940999d340408b2606588b922
hejunji <junji.he@biomind.ai>
2020-12-24 10:58:56 +0800

    数据库sequelize 优化 3, error fix

--- app/model/user.js
+++ app/model/user.js
@@ -1,27 +1,15 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  user.init({
-    name: DataTypes.STRING,
-    age: DataTypes.INTEGER,
-    avatar: DataTypes.STRING,
-    gender: DataTypes.STRING,
-    cellphone: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
-  return user;
+module.exports = (app) => {
+    const { STRING, INTEGER, DATE } = app.Sequelize;
+
+    const User = app.model.define('user', {
+        id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+        name: STRING(30),
+        age: INTEGER,
+        gender: STRING,
+        cellphone: STRING,
+        createdAt: DATE,
+        updatedAt: DATE
+    });
+    return User;
 };
